44edf9f9cd064d5ba90d9f66fc2f93c89792bb6f
ottojo <jottosmail@gmail.com>
2019-04-14 00:15:35 +0200

    add OTA update

--- toolboxDoor.ino
+++ toolboxDoor.ino
@@ -1,5 +1,6 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
+#include <ArduinoOTA.h>
 
 #define STASSID "Toolbox"
 #define STAPSK  ""
@@ -18,6 +19,7 @@ void setup() {
   Serial.print("Connecting to ");
   Serial.println(STASSID);
 
+  WiFi.hostname("DoorESP");
   WiFi.mode(WIFI_STA);
   WiFi.begin(STASSID, STAPSK);
 
@@ -30,14 +32,49 @@ void setup() {
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+
+  ArduinoOTA.setHostname("DoorESP");
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_SPIFFS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
 }
 
 void loop() {
   if (doorOpen != digitalRead(BUTTON_PIN)) {
     doorOpen = digitalRead(BUTTON_PIN);
     setDoorOpen(doorOpen);
+    delay(100);
   }
-  delay(100);
+  ArduinoOTA.handle();
 }
 
 void setDoorOpen(bool doorOpen) {
